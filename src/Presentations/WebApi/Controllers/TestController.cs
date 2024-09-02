5d144c04fab179c6e47dd43aebf11f496eb2ef9c
JoBeanO1 <jobenisawsome2000@gmail.com>
2024-09-02 12:26:30 -0500

    Create TestController.cs
    Joben created his first endpoint. :)

---
+++ src/Presentations/WebApi/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Data.Mongo.Collections;
+using Microsoft.AspNetCore.Mvc;
+using Models.DTOs.Log;
+using Models.ResponseModels;
+using Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("get")]
+        public string GetSampleString(string input)
+        {
+            string formattedText = $"You inputted {input}, and I think that's great";
+            return formattedText;
+        }
+    }
+}
